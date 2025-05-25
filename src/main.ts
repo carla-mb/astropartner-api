039de515540dc8bf9c9a6aaeeaf75f2dae47dbe5
Carla Martin <carlamartinbenito@gmail.com>
2025-05-25 19:22:59 +0200

    fix: update CORS configuration with deployed frontend

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   }
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['https://astropartner.netlify.app', 'http://localhost:4200'],
     methods: 'GET,POST,PUT,DELETE', 
     allowedHeaders: 'Content-Type, Authorization', 
     credentials: true, 
