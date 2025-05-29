43042b5045b03439eaff0086687146988bc910b1
Carla Martin <carlamartinbenito@gmail.com>
2025-05-29 20:50:24 +0200

    feat: update root endpoint with AstroPartner welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+      <p>AstroPartner is up and running!</p>
+      <p>Check it out here: <a href="https://astropartner.netlify.app/" target="_blank">AstroPartner</a></p>`;
   }
 }
